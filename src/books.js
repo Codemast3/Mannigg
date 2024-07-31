2196a041be99e9f3c9367acc64f8335cdba9a704
Codemast3r <gauravbhagat326@gmail.com>
2024-08-01 02:06:43 +0530

    chore: Remove unused files and update book list

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book1.jpg'
+import img2 from './images/img2.jpg'
+import img3 from './images/img3.jpg'
+
+export const books = [
+  {
+    title: 'Hillbilly Elegy: A Memoir of a Family and Culture in Crisis',
+    author: 'J.D. Vance',
+
+    id: 1,
+    img: img1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Colleen Hoover',
+    title: 'It Ends with Us: A Novel ',
+    img: img3,
+    id: 3,
+  },
+]
